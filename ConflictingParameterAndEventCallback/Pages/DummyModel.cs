01dddfc4a5685348483e0e145ad38b34e6055e29
JBBianchi <jb.bianchi@neuroglia.io>
2022-04-20 18:14:07 +0200

    Initial commit

---
+++ ConflictingParameterAndEventCallback/Pages/DummyModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConflictingParameterAndEventCallback.Pages
+{
+    public class DummyModel
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+        public DummyModel() { }
+        public DummyModel(string name) { 
+            this.Name = name;
+        }
+    }
+}
